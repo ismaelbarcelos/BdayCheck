4b501a521f904de609ec35667800c88c9e346d03
ismaelbarcelos <186837887+ismaelbarcelos@users.noreply.github.com>
2026-03-14 18:20:11 -0300

    update 1.0

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -6,7 +6,7 @@ export default function Home(){
 
 
 
- <input required value={valorData} onChange={e=>setData(e.target.value)} className="p-5 rounded-xl w-80 border-purple-300 border" type="date" name="" id="" />
+
 
 
 
